f89111fd207a0cefa415d9e7f61acf63b9bdc09e
Jacob Filik <jacob.filik@diamond.ac.uk>
2025-07-11 14:27:31 +0100

    make MUI

---
+++ src/TestComponent.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export default function TestComponent() {
+  return (
+    <Box flex={1}>
+      <Typography>A very boring test component</Typography>
+    </Box>
+  );
+}
