54a7ecfd23f44c3ec687405d4106d1af5dd63bf5
Subu Ghimire <254926418+subughimire420@users.noreply.github.com>
2026-09-04 17:00:51 +0100

    added PVs to all scans (#34)

--- src/EnergyScan.tsx
+++ src/EnergyScan.tsx
@@ -1,7 +1,9 @@
 import type { UseMutationResult } from "@tanstack/react-query";
-import { Button, Paper, Stack, TextField, Typography } from "@mui/material";
+import { Button, Paper, Stack, TextField, Typography, Accordion, AccordionSummary, AccordionDetails, Checkbox, FormControlLabel } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useState } from "react";
 
+type ReadablePV = [string, string, string, number, number, boolean, boolean, number];
 interface EnergyScanPlan {
   element: string,
   edge: string,
@@ -9,7 +11,35 @@ interface EnergyScanPlan {
   variable_exafs_time:boolean,
   number_of_sweeps:number,
   metadata: string,
+  enable_pvs: boolean;
+  pv_name: string;
+  pv_datatype: string;
+  pv_id: string;
+  pv_min_threshold : number,
+  pv_max_threshold: number,
+  pv_ensure_in_range: boolean,
+  pv_compute_snr: boolean,
+  pv_snr_min_threshold: number,
 }
+
+const parameterInfo: Record<string, string> = {
+  element: "element",
+  edge: "edge",
+  time_per_sweep:"time_per_sweep",
+  variable_exafs_time:"variable_exafs_time",
+  number_of_sweeps:"number_of_sweeps",
+  metadata: "Optional metadata",
+  enable_pvs:"Enable PVs",
+  pv_name:"Scannable PV",
+  pv_datatype:"Datatype of PV",
+  pv_id:"PV identifier",
+  pv_min_threshold : "Minimum Threshold",
+  pv_max_threshold: "Maximum Threshold",
+  pv_ensure_in_range: "Ensure this PV in range before running a scan",
+  pv_compute_snr: "Use SNR of this PV (True/False)",
+  pv_snr_min_threshold: "Minimum Required Threshold for SNR",
+};
+
 
 export default function EnergyScanPlanPanel(props: {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -24,43 +54,201 @@ export default function EnergyScanPlanPanel(props: {
     variable_exafs_time:false,
     number_of_sweeps:1,
     metadata:"",
+    enable_pvs:false,
+    pv_name:"BL51P-OP-PCHRO-01:TS:XFINE.RBV",
+    pv_datatype:"float",
+    pv_id: "motor_readback",
+    pv_min_threshold: -10,
+    pv_max_threshold: 20,
+    pv_ensure_in_range: false,
+    pv_compute_snr: false,
+    pv_snr_min_threshold: 30,
   });
+  const [readable_pvs, setPvs] = useState<ReadablePV[]>([]);
+  const handleChange = (key: keyof EnergyScanPlan, value: any) => {
+    setEnergyScanPlan(prev => ({ ...prev, [key]: value }));
+  };
+
   return (
     <Paper elevation={5}>
       <Stack height="100%" padding={"10px"} spacing={"20px"} overflow={"auto"}>
         <Typography>Energy Grid Scan</Typography>
         <Stack direction="column" spacing={"10px"}>
-          {Object.entries(EnergyScanPlan).map(([key, value]) => (
-            <TextField
-              key={key}
-              variant="outlined"
-              label={key}
-              type={typeof value === "number" ? "number" : "text"}
-              value={value}
-              onChange={(e) => {
-                setEnergyScanPlan({
-                  ...EnergyScanPlan,
-                  [key]:
-                    typeof value === "number"
-                      ? parseFloat(e.target.value)
-                      : e.target.value,
-                });
-              }}
-              slotProps={{ inputLabel: { shrink: true } }}
-            />
-          ))}
+          {[
+            "element",
+            "edge",
+            "time_per_sweep",
+            "variable_exafs_time",
+            "number_of_sweeps",
+            "metadata",
+          ]
+          .map((key) => {
+            const value = (EnergyScanPlan as any)[key]; // ✅ get value from plan
+            return (
+                <Stack direction="column" spacing={"10px"}>
+                  <Typography variant="body2" color="text.secondary">
+                    {parameterInfo[key] ?? "No description available"}
+                  </Typography>
+                  <TextField
+                    key={key}
+                    type={
+                      Array.isArray(value)
+                        ? "text"
+                        : typeof value === "number"
+                        ? "number"
+                        : "text"
+                    }
+                    value={Array.isArray(value) ? value.join(",") : value}
+                    onChange={(e) =>
+                      handleChange(
+                        key as any,
+                        Array.isArray(value)
+                          ? e.target.value
+                              .split(",")
+                              .map((v) => Number(v.trim()))
+                              .filter((v) => !Number.isNaN(v))
+                          : typeof value === "number"
+                          ? parseFloat(e.target.value)
+                          : e.target.value
+                      )
+                    }
+                  />
+                </Stack>
+              );
+            })}
+          
         </Stack>
+        
+        {/* PV Accordion */}
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>PV Settings</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Stack spacing={2}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={EnergyScanPlan.enable_pvs}
+                    onChange={(e) => handleChange("enable_pvs", e.target.checked)}
+                  />
+                }
+                label="Enable PVs"
+              />
+
+              {EnergyScanPlan.enable_pvs && (
+                <>
+                  {[
+                    "pv_name",
+                    "pv_datatype",
+                    "pv_id",
+                    "pv_min_threshold",
+                    "pv_max_threshold",
+                    "pv_ensure_in_range",
+                    "pv_compute_snr",
+                    "pv_snr_min_threshold",
+                  ].map((key) => {
+                    const value = (EnergyScanPlan as any)[key]; // ✅ get value from plan
+                    return (
+                      <Stack direction="column" spacing={"10px"}>
+                        <Typography variant="body2" color="text.secondary">
+                          {parameterInfo[key] ?? "No description available"}
+                        </Typography>
+                        <TextField
+                          key={key}
+                          type={
+                            Array.isArray(value)
+                              ? "text"
+                              : typeof value === "number"
+                              ? "number"
+                              : "text"
+                          }
+                          value={Array.isArray(value) ? value.join(",") : value}
+                          onChange={(e) =>
+                            handleChange(
+                              key as any,
+                              Array.isArray(value)
+                                ? e.target.value
+                                    .split(",")
+                                    .map((v) => Number(v.trim()))
+                                    .filter((v) => !Number.isNaN(v))
+                                : typeof value === "number"
+                                ? parseFloat(e.target.value)
+                                : e.target.value
+                            )
+                          }
+                        />
+                      </Stack>
+                    );
+                  })}
+
+                  <Button
+                    onClick={() => {
+                      setPvs(prev => [
+                        ...prev,
+                        [EnergyScanPlan.pv_id, EnergyScanPlan.pv_name, EnergyScanPlan.pv_datatype, EnergyScanPlan.pv_min_threshold, EnergyScanPlan.pv_max_threshold, EnergyScanPlan.pv_ensure_in_range, EnergyScanPlan.pv_compute_snr, EnergyScanPlan.pv_snr_min_threshold],
+                      ]);
+                    }}
+                  >
+                    Add PV
+                  </Button>
+                </>
+              )}
+            </Stack>
+          </AccordionDetails>
+          {readable_pvs.length > 0 && (
+            <Paper variant="outlined" sx={{ p: 2 }}>
+              <Typography>Readable PVs</Typography>
+              {readable_pvs.map((pv, i) => (
+                <Typography key={i}>{JSON.stringify(pv)}</Typography>
+              ))}
+            </Paper>
+          )}
+        </Accordion>
         <Button
           variant="contained"
           onClick={() => {
+            const {
+            metadata,
+            pv_id,
+            pv_name,
+            pv_datatype,
+            enable_pvs,
+            pv_min_threshold,
+            pv_max_threshold,
+            pv_ensure_in_range,
+            pv_compute_snr,
+            pv_snr_min_threshold,
+            ...rest
+            } = EnergyScanPlan;
+
+            const params: any = {
+                ...rest,
+                metadata: {
+                  user_comments: metadata,
+                },
+            };
+            
+            if (enable_pvs) {
+              params.readable_pvs = Object.fromEntries(
+                readable_pvs.map(pv => [
+                  pv[0],
+                  {
+                    pv_name: pv[1],
+                    pv_datatype: pv[2],
+                    pv_min_threshold : pv[3],
+                    pv_max_threshold: pv[4],
+                    pv_ensure_in_range: pv[5],
+                    pv_compute_snr: pv[6],
+                    pv_snr_min_threshold: pv[7],
+
+                  },
+                ])
+              );
+            }
             const toPost = {
               name: "seq_table_energy_scan",
-              params: {
-                    ...EnergyScanPlan,
-                    metadata: {
-                      user_comments: EnergyScanPlan.metadata
-                    },
-                  },
+              params,
               instrument_session: props.session,
             };
 
--- src/InterPandaTriggerScan.tsx
+++ src/InterPandaTriggerScan.tsx
@@ -1,7 +1,9 @@
 import type { UseMutationResult } from "@tanstack/react-query";
-import { Button, Paper, Stack, TextField, Typography } from "@mui/material";
+import { Button, Paper, Stack, TextField, Typography, Accordion, AccordionSummary, AccordionDetails, Checkbox, FormControlLabel } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useState } from "react";
 
+type ReadablePV = [string, string, string, number, number, boolean, boolean, number];
 
 type TriggerTuple = [
   number[], // trigger_output_ports
@@ -29,6 +31,16 @@ interface InterPandaTriggerPlan {
   trigger_output_num_repeats: number,
   trigger_type: number,
   trigger_repeat:number,
+  configure_sampleEnv_triggers: boolean, 
+  enable_pvs: boolean;
+  pv_name: string;
+  pv_datatype: string;
+  pv_id: string;
+  pv_min_threshold : number,
+  pv_max_threshold: number,
+  pv_ensure_in_range: boolean,
+  pv_compute_snr: boolean,
+  pv_snr_min_threshold: number,
 }
 
 const parameterInfo: Record<string, string> = {
@@ -42,12 +54,22 @@ const parameterInfo: Record<string, string> = {
   ramp_time: "Ramp up time (seconds)",
   turnaround_time: "Turnaround time (seconds)",
   metadata: "Optional metadata",
-  trigger_output_ports: "Output ports used for triggers in second Panda (comma-separated eg - 1,2,3)",
-  trigger_pulse_width: "Width of the trigger pulse in second Panda (seconds)",
-  trigger_output_delay: "Delay before trigger fires in second Panda (seconds)",
-  trigger_output_num_repeats: "How many times the trigger repeats in second Panda",
-  trigger_type: "Trigger mode identifier in second Panda (0 -> START, 1-> END)",
-  trigger_repeat: "Repeat the trigger sequence configured above in second Panda",
+  trigger_output_ports: "Output ports used for triggers (comma-separated eg - 1,2,3)",
+  trigger_pulse_width: "Width of the trigger pulse (seconds)",
+  trigger_output_delay: "Delay before trigger fires (seconds)",
+  trigger_output_num_repeats: "How many times the trigger repeats",
+  trigger_type: "Trigger mode (0 -> START, 1-> END)",
+  trigger_repeat: "Repeat the trigger sequence",
+  configure_sampleEnv_triggers: "Configure Sample Environment Triggers",
+  enable_pvs:"Enable PVs",
+  pv_name:"Scannable PV",
+  pv_datatype:"Datatype of PV",
+  pv_id:"PV identifier",
+  pv_min_threshold : "Minimum Threshold",
+  pv_max_threshold: "Maximum Threshold",
+  pv_ensure_in_range: "Ensure this PV in range before running a scan",
+  pv_compute_snr: "Use SNR of this PV (True/False)",
+  pv_snr_min_threshold: "Minimum Required Threshold for SNR",
 };
 
 
@@ -65,8 +87,8 @@ export default function InterPandaTriggerPlanPanel(props: {
     time_per_sweep: 5,
     num_trajectory_points: 10,
     add_sweep_triggers: false,
-    ramp_time: 0.00001,
-    turnaround_time: 0.00001,
+    ramp_time: 0.001,
+    turnaround_time: 0.0001,
     metadata:"",
     trigger_output_ports:[1,2], 
     trigger_pulse_width: 0.0001,
@@ -74,83 +96,263 @@ export default function InterPandaTriggerPlanPanel(props: {
     trigger_output_num_repeats: 1,
     trigger_type: 0,
     trigger_repeat:1,
+    configure_sampleEnv_triggers: false,
+    enable_pvs:false,
+    pv_name:"BL51P-OP-PCHRO-01:TS:XFINE.RBV",
+    pv_datatype:"float",
+    pv_id: "motor_readback",
+    pv_min_threshold: -10,
+    pv_max_threshold: 20,
+    pv_ensure_in_range: false,
+    pv_compute_snr: false,
+    pv_snr_min_threshold: 30,
   });
+  const [readable_pvs, setPvs] = useState<ReadablePV[]>([]);
+  const handleChange = (key: keyof InterPandaTriggerPlan, value: any) => {
+    setInterPandaTriggerPlan(prev => ({ ...prev, [key]: value }));
+  };
+
   const [triggers, setTriggers] = useState<TriggerTuple[]>([]);
   return (
     <Paper elevation={5}>
       <Stack height="100%" padding={"10px"} spacing={"20px"} overflow={"auto"}>
         <Typography>Inter Panda Triggering Scan</Typography>
-        <Stack direction="column" spacing={"10px"}>
-          {Object.entries(InterPandaTriggerPlan).map(([key, value]) => (
-            <Stack key={key} spacing={0.5}>
-              <Typography variant="body2" color="text.secondary">
-                {parameterInfo[key] ?? "No description available"}
-              </Typography>
-              <TextField
-                key={key}
-                variant="outlined"
-                type={typeof value === "number" ? "number" : "text"}
-                value={value}
-                onChange={(e) => {
-                  setInterPandaTriggerPlan({
-                    ...InterPandaTriggerPlan,
-                    [key]:Array.isArray(value)
-                    ? e.target.value
-                        .split(",")
-                        .map(v => Number(v.trim()))
-                        .filter(v => !Number.isNaN(v))
-                    : typeof value === "number"
-                        ? parseFloat(e.target.value)
-                        : e.target.value,
-                  });
-                }}
-                slotProps={{ inputLabel: { shrink: true } }}
-              />
-            </Stack>
-          ))}
-          <Button
-            variant="outlined"
-            onClick={() => {
-              const {
-                trigger_output_ports,
-                trigger_pulse_width,
-                trigger_output_delay,
-                trigger_output_num_repeats,
-                trigger_type,
-                trigger_repeat,
-              } = InterPandaTriggerPlan;
-
-              setTriggers(prev => [
-                ...prev,
-                [
-                  trigger_output_ports,
-                  trigger_pulse_width,
-                  trigger_output_delay,
-                  trigger_output_num_repeats,
-                  trigger_type,
-                  trigger_repeat,
-                ],
-              ]);
-            }}
-          >
-            Add Trigger
-          </Button>
-
-          {triggers.length > 0 && (
-            <Paper variant="outlined" sx={{ p: 2 }}>
-              <Typography variant="subtitle1">Triggers</Typography>
-
-              <Stack spacing={1}>
-                {triggers.map((trigger, index) => (
-                  <Typography key={index} variant="body2">
-                    Trigger {index + 1}:{" "}
-                    {JSON.stringify(trigger)}
+        {[
+        "start",
+        "stop",
+        "stepsize",
+        "number_of_sweeps",
+        "time_per_sweep",
+        "num_trajectory_points",
+        "ramp_time",
+        "turnaround_time",
+        "metadata",
+        ].map((key) => {
+            const value = (InterPandaTriggerPlan as any)[key]; // ✅ get value from plan
+            return (
+                <Stack direction="column" spacing={"10px"}>
+                  <Typography variant="body2" color="text.secondary">
+                    {parameterInfo[key] ?? "No description available"}
                   </Typography>
+                  <TextField
+                    key={key}
+                    type={
+                      Array.isArray(value)
+                        ? "text"
+                        : typeof value === "number"
+                        ? "number"
+                        : "text"
+                    }
+                    value={Array.isArray(value) ? value.join(",") : value}
+                    onChange={(e) =>
+                      handleChange(
+                        key as any,
+                        Array.isArray(value)
+                          ? e.target.value
+                              .split(",")
+                              .map((v) => Number(v.trim()))
+                              .filter((v) => !Number.isNaN(v))
+                          : typeof value === "number"
+                          ? parseFloat(e.target.value)
+                          : e.target.value
+                      )
+                    }
+                  />
+                </Stack>
+              );
+            })}
+
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={InterPandaTriggerPlan.add_sweep_triggers}
+              onChange={(e) => handleChange("add_sweep_triggers", e.target.checked)}
+            />
+          }
+          label="Enable Sweep Triggers"
+        />
+
+        {/* Trigger Accordion */}
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>Configure Sample Environment Triggers</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Stack spacing={2}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={InterPandaTriggerPlan.configure_sampleEnv_triggers}
+                    onChange={(e) => handleChange("configure_sampleEnv_triggers", e.target.checked)}
+                  />
+                }
+                label="Enable Triggers"
+              />
+
+              {InterPandaTriggerPlan.configure_sampleEnv_triggers && (
+                <>
+                  {[
+                    "trigger_output_ports",
+                    "trigger_pulse_width",
+                    "trigger_output_delay",
+                    "trigger_output_num_repeats",
+                    "trigger_type",
+                    "trigger_repeat"
+                  ].map((key) => {
+                    const value = (InterPandaTriggerPlan as any)[key]; // ✅ get value from plan
+
+                     return (
+                      <Stack direction="column" spacing={"10px"}>
+                        <Typography variant="body2" color="text.secondary">
+                          {parameterInfo[key] ?? "No description available"}
+                        </Typography>
+                        <TextField
+                          key={key}
+                          type={
+                            Array.isArray(value)
+                              ? "text"
+                              : typeof value === "number"
+                              ? "number"
+                              : "text"
+                          }
+                          value={Array.isArray(value) ? value.join(",") : value}
+                          onChange={(e) =>
+                            handleChange(
+                              key as any,
+                              Array.isArray(value)
+                                ? e.target.value
+                                    .split(",")
+                                    .map((v) => Number(v.trim()))
+                                    .filter((v) => !Number.isNaN(v))
+                                : typeof value === "number"
+                                ? parseFloat(e.target.value)
+                                : e.target.value
+                            )
+                          }
+                        />
+                      </Stack>
+                    );
+                  })}
+
+                  <Button
+                    onClick={() => {
+                      setTriggers(prev => [
+                        ...prev,
+                        [
+                          InterPandaTriggerPlan.trigger_output_ports,
+                          InterPandaTriggerPlan.trigger_pulse_width,
+                          InterPandaTriggerPlan.trigger_output_delay,
+                          InterPandaTriggerPlan.trigger_output_num_repeats,
+                          InterPandaTriggerPlan.trigger_type,
+                          InterPandaTriggerPlan.trigger_repeat,
+                        ],
+                      ]);
+                    }}
+                  >
+                    Add Trigger
+                  </Button>
+                </>
+              )}
+            </Stack>
+            {/* Preview */}
+            {triggers.length > 0 && (
+              <Paper variant="outlined" sx={{ p: 2 }}>
+                <Typography>Triggers</Typography>
+                {triggers.map((t, i) => (
+                  <Typography key={i}>{JSON.stringify(t)}</Typography>
                 ))}
-              </Stack>
+              </Paper>
+            )}            
+          </AccordionDetails>
+        </Accordion>
+
+        {/* PV Accordion */}
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>PV Settings</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Stack spacing={2}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={InterPandaTriggerPlan.enable_pvs}
+                    onChange={(e) => handleChange("enable_pvs", e.target.checked)}
+                  />
+                }
+                label="Enable PVs"
+              />
+
+              {InterPandaTriggerPlan.enable_pvs && (
+                <>
+                  {[
+                    "pv_name",
+                    "pv_datatype",
+                    "pv_id",
+                    "pv_min_threshold",
+                    "pv_max_threshold",
+                    "pv_ensure_in_range",
+                    "pv_compute_snr",
+                    "pv_snr_min_threshold",
+                  ].map((key) => {
+                    const value = (InterPandaTriggerPlan as any)[key]; // ✅ get value from plan
+                    return (
+                      <Stack direction="column" spacing={"10px"}>
+                        <Typography variant="body2" color="text.secondary">
+                          {parameterInfo[key] ?? "No description available"}
+                        </Typography>
+                        <TextField
+                          key={key}
+                          type={
+                            Array.isArray(value)
+                              ? "text"
+                              : typeof value === "number"
+                              ? "number"
+                              : "text"
+                          }
+                          value={Array.isArray(value) ? value.join(",") : value}
+                          onChange={(e) =>
+                            handleChange(
+                              key as any,
+                              Array.isArray(value)
+                                ? e.target.value
+                                    .split(",")
+                                    .map((v) => Number(v.trim()))
+                                    .filter((v) => !Number.isNaN(v))
+                                : typeof value === "number"
+                                ? parseFloat(e.target.value)
+                                : e.target.value
+                            )
+                          }
+                        />
+                      </Stack>
+                    );
+                  })}
+
+                  <Button
+                    onClick={() => {
+                      setPvs(prev => [
+                        ...prev,
+                        [InterPandaTriggerPlan.pv_id, InterPandaTriggerPlan.pv_name, InterPandaTriggerPlan.pv_datatype, InterPandaTriggerPlan.pv_min_threshold, InterPandaTriggerPlan.pv_max_threshold, InterPandaTriggerPlan.pv_ensure_in_range, InterPandaTriggerPlan.pv_compute_snr, InterPandaTriggerPlan.pv_snr_min_threshold],
+                      ]);
+                    }}
+                  >
+                    Add PV
+                  </Button>
+                </>
+              )}
+            </Stack>
+          </AccordionDetails>
+          {readable_pvs.length > 0 && (
+            <Paper variant="outlined" sx={{ p: 2 }}>
+              <Typography>Readable PVs</Typography>
+              {readable_pvs.map((pv, i) => (
+                <Typography key={i}>{JSON.stringify(pv)}</Typography>
+              ))}
             </Paper>
           )}
-        </Stack>
+        </Accordion>
+
         <Button
           variant="contained"
           onClick={() => {
@@ -162,6 +364,16 @@ export default function InterPandaTriggerPlanPanel(props: {
               trigger_output_num_repeats,
               trigger_type,
               trigger_repeat,
+              configure_sampleEnv_triggers,
+              pv_id,
+              pv_name,
+              pv_datatype,
+              enable_pvs,
+              pv_min_threshold,
+              pv_max_threshold,
+              pv_ensure_in_range,
+              pv_compute_snr,
+              pv_snr_min_threshold,
               ...rest
             } = InterPandaTriggerPlan;
 
@@ -173,6 +385,23 @@ export default function InterPandaTriggerPlanPanel(props: {
               };
 
             params.triggers = triggers;
+             if (enable_pvs) {
+              params.readable_pvs = Object.fromEntries(
+                readable_pvs.map(pv => [
+                  pv[0],
+                  {
+                    pv_name: pv[1],
+                    pv_datatype: pv[2],
+                    pv_min_threshold : pv[3],
+                    pv_max_threshold: pv[4],
+                    pv_ensure_in_range: pv[5],
+                    pv_compute_snr: pv[6],
+                    pv_snr_min_threshold: pv[7],
+
+                  },
+                ])
+              );
+            }
 
             const toPost = {
               name: "seq_table_two_panda_scan",
